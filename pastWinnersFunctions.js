4e96a9c6793eb666ba3d32b11a5742cfe3108860
pmathiasmcc <43451912+pmathiasmcc@users.noreply.github.com>
2018-12-05 10:39:14 -0500

    hopefully this will actually add the code when merged for the graphs in the table

--- pastWinnersFunctions.js
+++ pastWinnersFunctions.js
@@ -3,15 +3,156 @@ function openMenu() {
   document.getElementById("mySideMenu").style.width = "250px";
 }
 
+// function to display the graph each month when the table element is clicked
+// function openGraph(monthForGraph){}
+
 // Set width of side menu back to 0
 function closeMenu() {
   document.getElementById("mySideMenu").style.width = "0";
 }
 
+
+
 function openPioneer(month) {
   document.getElementById("pioneerSlideID").style.width = "1920px";
   document.getElementById("pioHeading").innerHTML = "Pioneer: " + month;
+
+  /*Get line canvas for line graph*/
+  let pioneerChart = document.getElementById('pioneerChart').getContext('2d');
+
+
+  //Global options
+  Chart.defaults.global.defaultFontFamily = 'Segoe UI';
+  Chart.defaults.global.defaultFontSize = 18;
+  Chart.defaults.global.defaultColor = 'rgb(0, 0, 102,1)'; //this changes the color of the inside of the legend logos
+
+
+
+  /* build line chart */
+  let pioneerLineChart = new Chart(pioneerChart, {
+
+    type:'line', // declares it a line chart
+    data:{
+      labels:['Day 1', 'Day 2', 'Day 3', 'Day 4', 'Day 5', 'Day 6', 'Day 7', 'Day 8', 'Day 9',
+      'Day 10', 'Day 11'], // chose 10 3 day interval dates for demo; added Oct. 1 for starting point
+
+      datasets:[
+        {
+          label:'Pioneer',
+          data:[
+            1.00,
+            3.98,
+            4.32,
+            4.67,
+            5.70,
+            5.99,
+            6.54,
+            7.01,
+            7.85,
+            8.78,
+            12.20
+          ],
+          fill:false,
+          borderColor:'rgb(0, 204, 204)', // sets color of this line to dark grey
+          backgroundColor: 'rgba(102, 255, 255, 1)', // background color of fill lines
+          pointBackgroundColor:'rgb(169, 169, 169, 0.9)', // gets rid of the backgroundColor pixel in the center of the point
+          borderWidth:5,
+          hoverBorderWidth:3,
+          hoverBorderColor:'#000',
+        },
+      ],
+    },
+
+    options:{
+
+      maintainAspectRatio: true,
+      responsive: true,
+
+      title:{
+        display:false, // set to false because it will hopefully be evident, but can change
+        text:'BUILDING NAME',
+        fontColor: '#cff',
+        fontSize:25,
+      },
+
+      legend:{
+        display: false,
+        position:'top',  // can do top, bottom, left, right
+        cursor: 'pointer',
+        padding:{
+          left:100, // don't know if this actually affected it
+        },
+
+        labels:{
+          fontColor:'#cff',
+          fontStyle:'bold',
+        },
+      },
+
+      layout:{
+        padding:{ // all values to 0 so this doesn't change anything
+        left:0,
+        right:0,
+        bottom:0,
+        top:0,
+      },
+    },
+
+    scales:{
+
+      xAxes: [{
+        ticks:{
+          fontColor: '#cff',
+          max:'Oct. 31', // sets max of y axis to 16 - consider using suggestedMax when actual data comes
+          min:'Oct. 1', // sets min of x axis to 0
+        },
+
+        gridLines:{
+          color:'#cff',
+          lineWidth:3, // changes width of x axis gridline on the side
+          display:false, // gets rid of x axis gridlines
+          //color: "rgba(0, 0, 0, 0)",  from when I didn't know display:false was a thing
+        },
+      }],
+
+      yAxes: [{
+        ticks:{
+          fontColor: '#cff',
+          max:16, // sets max of y axis to 16 - consider using suggestedMax when actual data comes
+          min:0, // sets min of x axis to 0
+        },
+
+        gridLines:{
+          color:'#cff',
+          lineWidth:3, // changes width of y axis gridline on the side
+          display:false, // gets rid of y axis gridlines
+          //  color: "rgba(0, 0, 0, 0)",  from when I didn't know display:false was a thing
+        },
+
+        scaleLabel:{
+          display:true, // will display title of y axis
+          labelString:'Water Usage (1000s of gallons)', // title that is displayed
+          fontColor: '#cff',
+          fontStyle:'bold',
+          padding:{
+            bottom:10, // adds a little space between axis and title
+          },
+        },
+      }],
+    },
+  },
+  });
+
+  //declare the rgb values of red, green and blue in chart colors
+  window.chartColors = {
+    basset: 'rgb(51, 102, 255)',
+    pioneer: 'rgb(0, 255, 255)',
+    dv: 'rgb(128, 255, 191)',
+  };
+
 }
+
+
 
 function closePioneer() {
   document.getElementById("pioneerSlideID").style.width = "0";
@@ -20,6 +161,140 @@ function closePioneer() {
 function openBasset(month) {
   document.getElementById("bassetSlideID").style.width = "1920px";
   document.getElementById("basHeading").innerHTML = "Basset: " + month;
+
+  /*Get line canvas for line graph*/
+  let bassetChart = document.getElementById('bassetChart').getContext('2d');
+
+
+  //Global options
+  Chart.defaults.global.defaultFontFamily = 'Segoe UI';
+  Chart.defaults.global.defaultFontSize = 18;
+  Chart.defaults.global.defaultColor = 'rgb(0, 0, 102,1)'; //this changes the color of the inside of the legend logos
+
+
+
+  /* build line chart */
+  let bassetLineChart = new Chart(bassetChart, {
+
+    type:'line', // declares it a line chart
+    data:{
+      labels:['Day 1', 'Day 2', 'Day 3', 'Day 4', 'Day 5', 'Day 6', 'Day 7', 'Day 8', 'Day 9',
+      'Day 10', 'Day 11'], // chose 10 3 day interval dates for demo; added Oct. 1 for starting point
+
+      datasets:[
+        {
+          label:'Bassett',
+          data:[
+            1.99,
+            5.55,
+            2.15,
+            3.92,
+            8.14,
+            8.85,
+            3.25,
+            4.18,
+            3.18,
+            6.66,
+            12.20
+          ],
+          fill:false,
+          borderColor:'rgb(0, 204, 204)', // sets color of this line to dark grey
+          backgroundColor: 'rgba(102, 255, 255, 1)', // background color of fill lines
+          pointBackgroundColor:'rgb(169, 169, 169, 0.9)', // gets rid of the backgroundColor pixel in the center of the point
+          borderWidth:5,
+          hoverBorderWidth:3,
+          hoverBorderColor:'#000',
+        },
+      ],
+    },
+
+    options:{
+
+      maintainAspectRatio: true,
+      responsive: true,
+
+      title:{
+        display:false, // set to false because it will hopefully be evident, but can change
+        text:'BUILDING NAME',
+        fontColor: '#cff',
+        fontSize:25,
+      },
+
+      legend:{
+        display: false,
+        position:'top',  // can do top, bottom, left, right
+        cursor: 'pointer',
+        padding:{
+          left:100, // don't know if this actually affected it
+        },
+
+        labels:{
+          fontColor:'#cff',
+          fontStyle:'bold',
+        },
+      },
+
+      layout:{
+        padding:{ // all values to 0 so this doesn't change anything
+        left:0,
+        right:0,
+        bottom:0,
+        top:0,
+      },
+    },
+
+    scales:{
+
+      xAxes: [{
+        ticks:{
+          fontColor: '#cff',
+          max:'Oct. 31', // sets max of y axis to 16 - consider using suggestedMax when actual data comes
+          min:'Oct. 1', // sets min of x axis to 0
+        },
+
+        gridLines:{
+          color:'#cff',
+          lineWidth:3, // changes width of x axis gridline on the side
+          display:false, // gets rid of x axis gridlines
+          //color: "rgba(0, 0, 0, 0)",  from when I didn't know display:false was a thing
+        },
+      }],
+
+      yAxes: [{
+        ticks:{
+          fontColor: '#cff',
+          max:16, // sets max of y axis to 16 - consider using suggestedMax when actual data comes
+          min:0, // sets min of x axis to 0
+        },
+
+        gridLines:{
+          color:'#cff',
+          lineWidth:3, // changes width of y axis gridline on the side
+          display:false, // gets rid of y axis gridlines
+          //  color: "rgba(0, 0, 0, 0)",  from when I didn't know display:false was a thing
+        },
+
+        scaleLabel:{
+          display:true, // will display title of y axis
+          labelString:'Water Usage (1000s of gallons)', // title that is displayed
+          fontColor: '#cff',
+          fontStyle:'bold',
+          padding:{
+            bottom:10, // adds a little space between axis and title
+          },
+        },
+      }],
+    },
+  },
+  });
+
+  //declare the rgb values of red, green and blue in chart colors
+  window.chartColors = {
+    basset: 'rgb(51, 102, 255)',
+    pioneer: 'rgb(0, 255, 255)',
+    dv: 'rgb(128, 255, 191)',
+  };
+
 }
 
 function closeBasset() {
